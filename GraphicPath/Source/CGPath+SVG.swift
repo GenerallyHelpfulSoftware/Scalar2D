49f7bde086c3db5c79567afba572407d557e9087
Glenn Howes <grhowes@mac.com>
2016-08-10 07:34:20 -0400

    MIT License notices.

--- GraphicPath/Source/CGPath+SVG.swift
+++ GraphicPath/Source/CGPath+SVG.swift
@@ -5,6 +5,29 @@
 //  Created by Glenn Howes on 8/7/16.
 //  Copyright © 2016 Generally Helpful Software. All rights reserved.
 //
+// The MIT License (MIT)
+
+//  Copyright (c) 2016 Glenn R. Howes
+
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+//
 
 import Foundation
 import CoreGraphics
@@ -21,7 +44,6 @@ fileprivate struct Vector2D
         self.deltaY = deltaY
     }
 
-    // misusing CGPoint as a vector for laziness
     private var vectorMagnitude: CGFloat
     {
         let	result = sqrt(self.deltaX*self.deltaX+self.deltaY*self.deltaY)
